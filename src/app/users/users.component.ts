8e06b61835596fbfd6d42b083d0da53e879e9090
DarkmanTMR <trp1prt1@gmail.com>
2021-07-24 18:45:50 +0300

    Units - unit - model

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {IUserModel} from "../models/UserModel";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent {
+
+ users: IUserModel[] = [                                        //прив'язка масиву до моделі
+  {name: 'vasya', age: 31, status: false},
+  {name: 'petya', age: 30, status: true},
+  {name: 'kolya', age: 29, status: true},
+  {name: 'olya', age: 28, status: false},
+  {name: 'max', age: 30, status: true},
+  {name: 'anya', age: 31, status: false},
+  {name: 'oleg', age: 28, status: false},
+  {name: 'andrey', age: 29, status: true},
+  {name: 'masha', age: 30, status: true},
+  {name: 'olya', age: 31, status: false},
+  {name: 'max', age: 31, status: true}
+];
+
+
+  constructor() {
+
+  }
+
+}
