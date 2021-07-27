5deb2b1b389027eb1ddb65110ba0f1d6e72443b3
DarkmanTMR <trp1prt1@gmail.com>
2021-07-27 20:27:34 +0300

    Users with details

---
+++ src/app/routes/basic.routes.ts
@@ -0,0 +1,18 @@
+import {UsersComponent} from "../users/users.component";
+import {PostsComponent} from "../posts/posts.component";
+import {UserDetailsComponent} from "../user-details/user-details.component";
+import {Routes} from "@angular/router";
+
+export let routes: Routes = [
+  {
+    path:'users',component: UsersComponent,
+    children : [
+      {path: ':id', component: UserDetailsComponent}
+    ]
+
+  },
+  // {path:'users/:id',component: UserDetailsComponent}, дає можливість дивитися деталі кожного юзера окремо
+
+
+  {path:'posts',component: PostsComponent},
+];
