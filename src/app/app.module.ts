8ca766923cc174a3b8d38f01a2cf1dd569081a42
DarkmanTMR <trp1prt1@gmail.com>
2021-08-01 17:49:14 +0300

    posts part done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import {RouterModule} from "@angular/router";
 import { UsersComponent } from './components/users/users.component';
 import {routes} from "./routes/routes";
@@ -9,6 +9,8 @@ import {HttpClientModule} from "@angular/common/http";
 import { PostsComponent } from './components/posts/posts.component';
 import { UserComponent } from './components/user/user.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PostComponent } from './components/post/post.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 
 
 @NgModule({
@@ -17,7 +19,9 @@ import { UserDetailsComponent } from './components/user-details/user-details.com
     UsersComponent,
     PostsComponent,
     UserComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    PostComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -2,11 +2,16 @@ import {Routes} from "@angular/router";
 import {UsersComponent} from "../components/users/users.component";
 import {UserDetailsComponent} from "../components/user-details/user-details.component";
 import {UserDeactivatorService} from "../services/user-deactivator.service";
+import {PostDetailsComponent} from "../components/post-details/post-details.component";
+import {PostsComponent} from "../components/posts/posts.component";
 
 export let routes: Routes = [
-  {path: '', component: UsersComponent,canDeactivate:[UserDeactivatorService]},
-  {path: 'users', redirectTo: '', pathMatch: 'full'},
-  {path: 'users/:id', component: UserDetailsComponent}
+  {path: 'users', component: UsersComponent,canDeactivate:[UserDeactivatorService]},
+  // {path: 'users', redirectTo: '', pathMatch: 'full'},
+  {path: 'users/:id', component: UserDetailsComponent},
+  {path: 'posts', component: PostsComponent},
+  // {path: 'posts', redirectTo: '', pathMatch: 'full'},
+  {path: 'posts/:id', component: PostDetailsComponent}
 ];
 
 
